9477eadebb75fa29411f5541ce6a545b02948e05
Antoine <24255551+antoineverin@users.noreply.github.com>
2024-11-04 16:10:59 +0100

    add p2

---
+++ p2/scripts/server.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+ufw disable
+
+curl -sfL https://get.k3s.io | sh -s -
+
+sudo mkdir -p /home/vagrant/.kube
+sudo cp /etc/rancher/k3s/k3s.yaml /home/vagrant/.kube/config
+sudo chown -R vagrant:vagrant /home/vagrant/.kube/config
+
+kubectl apply -f confs
