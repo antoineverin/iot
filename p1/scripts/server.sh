b5415342a18532ee5c3c91e284375d5e76de0e13
Antoine <24255551+antoineverin@users.noreply.github.com>
2024-11-07 11:14:11 +0100

    remove unnecessary commands

--- p1/scripts/server.sh
+++ p1/scripts/server.sh
@@ -5,8 +5,4 @@ apt-get install -y curl
 
 curl -sfL https://get.k3s.io | sh -s -
 
-sudo mkdir -p /home/vagrant/.kube
-sudo cp /etc/rancher/k3s/k3s.yaml /home/vagrant/.kube/config
-sudo chown -R vagrant:vagrant /home/vagrant/.kube/config
-
 sudo cat /var/lib/rancher/k3s/server/node-token > /vagrant/token
